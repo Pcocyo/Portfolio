d4af639b62748ff4da8c2221e3962828f2413445
Pcocyo <joshuaelson46@gmail.com>
2025-04-27 00:19:24 +0800

    (feat): technology setups framer-motion react react-dom tailwind typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { motion } from 'framer-motion'
-import { useState } from 'react';
-
-import AnimateButton from './testFolder/AnimateButton';
-//import MovingA from './testFolder/MovingA';
-//import LittleTest from './testFolder/LittleTest';
-//import LinkHoverTest from './testFolder/LinkHoverTest';
-import TestAll from './testFolder/TestAll';
 function App() {
   return (
     <> 
-        <TestAll></TestAll>     
+      App
     </>
   )
 }
