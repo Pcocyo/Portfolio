4f65e33001f4125bcc56ed59f7e275a98477330c
Pcocyo <joshuaelson46@gmail.com>
2025-04-27 20:19:31 +0800

    (fix): remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React,{useState} from 'react';
+import React from 'react';
 import IconNav from './component/IconNav/IconNav';
 import ContextProvider from './ContextProvider';
 import TextNav from './component/TextNav/TextNav';
 
 const bodyStyle:string = 'h-[fit] bg-[#040B15] text-[#f5f5f5]'
+
 const App:React.FC = ()=>{
   return (
     <div 
