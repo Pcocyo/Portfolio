a3ae7bd7ca1d45ad75b33845e2c54271c6b22bf5
Pcocyo <joshuaelson46@gmail.com>
2025-04-30 20:14:02 +0800

    feat(App.tsx) Update App.tsx to enable new feature in AboutPage, TextNav and etc

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,27 @@ import React from 'react';
 import IconNav from './component/IconNav/IconNav';
 import ContextProvider from './ContextProvider';
 import TextNav from './component/TextNav/TextNav';
+import AboutPage from './component/AboutPage/AboutPage';
+const bodyStyle: string = 'h-[fit] bg-[#040B15] text-[#f5f5f5] w-[100%]'
 
-const bodyStyle:string = 'h-[fit] bg-[#040B15] text-[#f5f5f5]'
-
-const App:React.FC = ()=>{
+const App: React.FC = () => {
   return (
-    <div 
-    className={bodyStyle}
+    <div
+      className={bodyStyle}
     >
       <ContextProvider>
-        <IconNav/>
-        <div className="h-[1200px] border border-green-500 static">
-          <TextNav/>
-          <div className="">test</div>
-          <div className="">test</div>
-        </div>
+        <IconNav className="h-[50px] flex items-center p-[14px] justify-between overflow-hidden sticky top-0 bg-[#02060C] z-[1]"/>
+        <main className="h-[fit] static overflow-auto z-[0]">
+          <section>
+            <AboutPage className="h-[70vh] lg:h-fit"/>
+            <div className="h-[100vh]" id="Project">
+               Project
+            </div>
+          </section>
+          <aside>
+            <TextNav className='hidden md:h-[60vh] md:w-[50px] md:fixed md:top-[25%] md:right-0 md:flex md:flex-col md:justify-around md:items-end md:mr-[14px]' />
+          </aside>
+        </main>
       </ContextProvider>
     </div>
   )
